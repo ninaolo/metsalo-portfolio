c0c2f8886104eea965996f6777bc348e98d3bd0f
Nina Olofsson <Nina@Ninas-MacBook-Pro.local>
2017-10-29 18:18:15 +0100

    fea(home) Update home

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,30 @@
-import React from 'react'
-import { Switch, Route } from 'react-router-dom'
+import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
+import chilitomte from "./assets/chilitomte.jpg";
+import "./Home.css";
 
-const Home = () => (
-  <p>Hejsan från home.</p>
-)
+class Home extends Component {
+	render() {
+		return (
+			<p>Hej</p>
+			/*
+			<div className="row">
+				<div className="col-lg-8 offset-lg-2">
 
-export default Home
+					<div className="img-container">
+						<a href="#">
+						<img src={chilitomte} className="image" />
+						<div className="middle">
+							<div className="text">Chilitomten</div>
+						</div>
+						</a>
+						<h4>Christmas card Chilitomten</h4>
+						<p>Hejsan från home.</p>
+					</div>
+				</div>
+			</div>*/
+		);
+	}
+}
+
+export default Home;
